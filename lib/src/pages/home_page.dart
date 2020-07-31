74e6bb3e1d45ea2628ab029c5b75c8393672a600
yayorome <romero.yair@gmail.com>
2020-07-30 19:39:48 -0500

    Navbar

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:qr_sqlite/src/pages/directions_page.dart';
+import 'package:qr_sqlite/src/pages/maps_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentPage = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Text'),
-      ),
+      body: _loadPage(currentPage),
+      bottomNavigationBar: _bottonNavbar(),
     );
+  }
+
+  Widget _loadPage(int actualPage) {
+    switch (actualPage) {
+      case 0:
+        return MapsPage();
+      case 1:
+        return DirectionsPage();
+      default:
+        return MapsPage();
+    }
+  }
+
+  Widget _bottonNavbar() {
+    return BottomNavigationBar(
+        currentIndex: currentPage,
+        onTap: (index) {
+          setState(() {
+            currentPage = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.map), title: Text('Mapas')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.brightness_5), title: Text('Direcciones'))
+        ]);
   }
 }
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:qr_sqlite/src/pages/directions_page.dart';
 import 'package:qr_sqlite/src/pages/home_page.dart';
+import 'package:qr_sqlite/src/pages/maps_page.dart';
 
 Map<String, WidgetBuilder> getAppRoutes() {
   return <String, WidgetBuilder>{
     '/': (BuildContext context) => HomePage(),
+    'maps': (BuildContext context) => MapsPage(),
+    'directions': (BuildContext context) => DirectionsPage(),
   };
 }
